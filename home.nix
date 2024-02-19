fd4dba558aa52296cc07673266f5fdf1401fddf0
Menno van Leeuwen <menno@vleeuwen.me>
2024-02-19 18:09:37 +0100

    wip
    Signed-off-by: Menno van Leeuwen <menno@vleeuwen.me>

--- home.nix
+++ home.nix
@@ -1,7 +1,6 @@
-{ config, pkgs, ... }:
-{
+{ config, lib, pkgs, ... }:
+{       
   fonts.fontconfig.enable = true;
-
   home = {
     username = "menno";
     homeDirectory = "/home/menno";
@@ -15,6 +14,7 @@
       lsd
       zsh
       zsh-powerlevel10k
+      fzf
     ];
   };
 
@@ -33,6 +33,17 @@
       userName = "Menno van Leeuwen";
       userEmail = "menno@vleeuwen.me";
       includes = [ { path = "~/.dotfiles/config/gitconfig"; } ];
+    };
+
+    ssh = {
+      enable = true;
+      matchBlocks = {
+        server = {
+          port = 22;
+          hostname =  "192.168.86.254";
+          user = "menno";
+        };
+      };
     };
 
     zsh = {
