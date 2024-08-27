63a194855101a61e3a51ef4734d9c39f49a8aa0b
Menno van Leeuwen <menno@vleeuwen.me>
2024-08-27 20:52:24 +0200

    feat: Add push command to automate pushing changes to all remotes

---
+++ bin/actions/push.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+
+source $HOME/dotfiles/bin/helpers/functions.sh
+
+# Push all changes from $HOME/dotfiles to ALL remotes in $HOME/dotfiles/.git/config
+push_all() {
+    # Get all remotes from the .git/config file
+    remotes=($(cat $HOME/dotfiles/.git/config | grep url | awk '{print $3}'))
+
+    printfe "%s\n" "cyan" "Pushing all changes to all remotes..."
+
+    # For each remote, push all changes
+    for remote in $remotes; do
+        printfe "%s" "green" "  - Pushing to ["
+        printfe "%s" "blue" "$remote"
+        printfe "%s\n" "green" "]..."
+
+        result=$(git -C $HOME/dotfiles push $remote 2>&1)
+
+        # If the push failed, print an error
+        if [ $? -ne 0 ]; then
+            printfe "%s\n" "red" "    - Failed to push to $remote:"
+            printfe "%s\n" "red" "      $result"
+            continue
+        fi
+    done
+}
+
+push_all
