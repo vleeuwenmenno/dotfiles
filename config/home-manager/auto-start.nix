3221f541f46c64583026768f9c0a74efbb7f9081
Menno van Leeuwen <menno@vleeuwen.me>
2024-10-30 23:21:58 +0100

    fix: start apps only on graphical not before

--- config/home-manager/auto-start.nix
+++ config/home-manager/auto-start.nix
@@ -4,7 +4,8 @@
     onepassword-autostart = {
       Unit = {
         Description = "Start 1Password on login";
-        After = [ "graphical-session-pre.target" ];
+        # Change 'After' to use 'graphical-session.target' directly
+        After = [ "graphical-session.target" ];
         PartOf = [ "graphical-session.target" ];
       };
       Service = {
@@ -12,14 +13,14 @@
         Restart = "no";
       };
       Install = {
-        WantedBy = [ "graphical-session.target" ];
+        WantedBy = [ "default.target" ];  # Target used for generic user services
       };
     };
 
     trayscale-autostart = {
       Unit = {
         Description = "Start Trayscale on login";
-        After = [ "graphical-session-pre.target" ];
+        After = [ "graphical-session.target" ];
         PartOf = [ "graphical-session.target" ];
       };
       Service = {
@@ -27,14 +28,14 @@
         Restart = "no";
       };
       Install = {
-        WantedBy = [ "graphical-session.target" ];
+        WantedBy = [ "default.target" ];
       };
     };
 
     vesktop-autostart = {
       Unit = {
         Description = "Start Vesktop on login";
-        After = [ "graphical-session-pre.target" ];
+        After = [ "graphical-session.target" ];
         PartOf = [ "graphical-session.target" ];
       };
       Service = {
@@ -42,14 +43,14 @@
         Restart = "no";
       };
       Install = {
-        WantedBy = [ "graphical-session.target" ];
+        WantedBy = [ "default.target" ];
       };
     };
 
     telegram-autostart = {
       Unit = {
         Description = "Start Telegram Desktop on login";
-        After = [ "graphical-session-pre.target" ];
+        After = [ "graphical-session.target" ];
         PartOf = [ "graphical-session.target" ];
       };
       Service = {
@@ -57,14 +58,29 @@
         Restart = "no";
       };
       Install = {
-        WantedBy = [ "graphical-session.target" ];
+        WantedBy = [ "default.target" ];
+      };
+    };
+
+    whatsapp-for-linux-autostart = {
+      Unit = {
+        Description = "Start WhatsApp for Linux on login";
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.whatsapp-for-linux}/bin/whatsapp-for-linux";
+        Restart = "no";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
       };
     };
 
     spotify-autostart = {
       Unit = {
         Description = "Start Spotify on login";
-        After = [ "graphical-session-pre.target" ];
+        After = [ "graphical-session.target" ];
         PartOf = [ "graphical-session.target" ];
       };
       Service = {
@@ -72,7 +88,7 @@
         Restart = "no";
       };
       Install = {
-        WantedBy = [ "graphical-session.target" ];
+        WantedBy = [ "default.target" ];
       };
     };
   };
