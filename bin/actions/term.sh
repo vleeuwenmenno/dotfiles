ec468d62218e32df7586eb0b27286d951f99a547
Menno van Leeuwen <menno@vleeuwen.me>
2024-11-03 22:11:30 +0100

    adds nice welcome log to show where you are logged in on

--- bin/actions/term.sh
+++ bin/actions/term.sh
@@ -2,4 +2,19 @@
 
 source $HOME/dotfiles/bin/helpers/functions.sh
 
+welcome() {
+  echo
+  tput setaf 6
+  printf "You're logged in on ["
+  printf $HOSTNAME | lolcat
+  tput setaf 6
+  printf "] as "
+  printf "["
+  printf $USER | lolcat
+  tput setaf 6
+  printf "]\n"
+  tput sgr0
+}
+
 logo continue
+welcome
