d0ce652f04079b42d8cff67def23852c8ebc5e3a
Menno van Leeuwen <menno@vleeuwen.me>
2024-11-05 23:29:09 +0100

    renames vpn service to wireguard

---
+++ config/nixos/docker/wireguard.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  environment.etc."docker/wireguard".source = ./wireguard;
+
+  systemd.services.wireguard = {
+    description = "Wireguard Docker Compose Service";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.docker-compose}/bin/docker-compose -f /etc/docker/wireguard/docker-compose.yml up";
+      ExecStop = "${pkgs.docker-compose}/bin/docker-compose -f /etc/docker/wireguard/docker-compose.yml down";
+      WorkingDirectory = "/etc/docker/wireguard";
+      Restart = "always";
+      RestartSec = 10;
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+}
