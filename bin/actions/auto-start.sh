b12fa46ea1db20688eb2546ca5cd609b3d8280e1
Menno van Leeuwen <menno@vleeuwen.me>
2024-11-11 11:26:20 +0100

    re-added auto-start and updated auto-start to check for existing sessions

--- bin/actions/auto-start.sh
+++ bin/actions/auto-start.sh
@@ -7,11 +7,18 @@ apps=(
   vesktop
   trayscale
   1password
+  fcitx5
 )
 
 echo "Starting auto-start applications..."
 for app in "${apps[@]}"; do
   if [ -x "$(command -v $app)" ]; then
+    # Check if there's already a screen session with the same name
+    if screen -list | grep -q $app; then
+      echo "$app is already running. Skipping..."
+      continue
+    fi
+    
     echo "Starting $app..."
     screen -dmS $app $app
     sleep 1
