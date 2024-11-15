1e73386dca2033ab16afb418468320e2523b4f5c
Menno van Leeuwen <menno@vleeuwen.me>
2024-11-16 00:05:42 +0100

    disabled nginx for now
    Signed-off-by: Menno van Leeuwen <menno@vleeuwen.me>

--- config/nixos/packages/server/nginx.nix
+++ config/nixos/packages/server/nginx.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 {
   # Resume/CV Website (Nginx)
-  services.nginx = {
-    enable = true;
-    virtualHosts."localhost:4203" = {
-      root = "/home/menno/dotfiles/config/nixos/packages/server/mennovanleeuwen.nl";
-      locations."/" = {
-        index = "index.html";
-      };
-    };
-  };
+  # services.nginx = {
+  #   enable = true;
+  #   virtualHosts."localhost:4203" = {
+  #     root = "/home/menno/dotfiles/config/nixos/packages/server/mennovanleeuwen.nl";
+  #     locations."/" = {
+  #       index = "index.html";
+  #     };
+  #   };
+  # };
 }
