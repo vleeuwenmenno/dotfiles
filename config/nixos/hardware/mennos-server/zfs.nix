1937bd9acf4dd826477df063b4b691d08c33276a
Menno van Leeuwen <menno@vleeuwen.me>
2024-11-15 22:48:09 +0100

    adds zfs mount points

--- config/nixos/hardware/mennos-server/zfs.nix
+++ config/nixos/hardware/mennos-server/zfs.nix
@@ -19,4 +19,72 @@
 
   # If you want to keep compression settings
   boot.kernelParams = [ "zfs.zfs_compressed_arc_enabled=1" ];
+
+  fileSystems = {
+    "/mnt/ai" = {
+      device = "datapool/ai";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/astrophotography" = {
+      device = "datapool/astro";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/audiobooks" = {
+      device = "datapool/audiobooks";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/downloads" = {
+      device = "datapool/downloads";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/ISOs" = {
+      device = "datapool/isos";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/movies" = {
+      device = "datapool/movies";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/music" = {
+      device = "datapool/music";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/old_backups" = {
+      device = "datapool/old_backups";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/photos" = {
+      device = "datapool/photos";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/services" = {
+      device = "datapool/services";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/stash" = {
+      device = "datapool/stash";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/tvshows" = {
+      device = "datapool/tv_shows";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+    "/mnt/VMs" = {
+      device = "datapool/vms";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+  };
 }
