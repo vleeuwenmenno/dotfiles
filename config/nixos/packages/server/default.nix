0e619da20785e58d166fd1d7c1748a70255e9b6d
Menno van Leeuwen <menno@vleeuwen.me>
2024-11-15 20:46:36 +0100

    temporarily disabled zfs

--- config/nixos/packages/server/default.nix
+++ config/nixos/packages/server/default.nix
@@ -1,7 +1,10 @@
 { config, pkgs, ... }:
 
 let
-  files = builtins.removeAttrs (builtins.readDir ./.) [ "default.nix" ];
+  files = builtins.removeAttrs (builtins.readDir ./.) [
+    "default.nix"
+    "mennovanleeuwen.nl"
+  ];
 
   # Import all other .nix files as modules
   moduleFiles = builtins.map (fname: ./. + "/${fname}") (builtins.attrNames files);
