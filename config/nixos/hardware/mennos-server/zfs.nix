e719b0e69363575c368f3d44d3cc6423b2870377
Menno van Leeuwen <menno@vleeuwen.me>
2024-11-15 21:23:36 +0100

    disabled zfs mount points

--- config/nixos/hardware/mennos-server/zfs.nix
+++ config/nixos/hardware/mennos-server/zfs.nix
@@ -21,60 +21,60 @@
 
   # Your ZFS pool and datasets will be automatically imported
   # But we can specify mount points explicitly for clarity
-  fileSystems = {
-    "/mnt/ai" = {
-      device = "datapool/ai";
-      fsType = "zfs";
-    };
-    "/mnt/movies" = {
-      device = "datapool/movies";
-      fsType = "zfs";
-    };
-    "/mnt/tvshows" = {
-      device = "datapool/tv_shows";
-      fsType = "zfs";
-    };
-    "/mnt/music" = {
-      device = "datapool/music";
-      fsType = "zfs";
-    };
-    "/mnt/astrophotography" = {
-      device = "datapool/astro";
-      fsType = "zfs";
-    };
-    "/mnt/downloads" = {
-      device = "datapool/downloads";
-      fsType = "zfs";
-    };
-    "/mnt/photos" = {
-      device = "datapool/photos";
-      fsType = "zfs";
-    };
-    "/mnt/stash" = {
-      device = "datapool/stash";
-      fsType = "zfs";
-    };
-    "/mnt/ISOs" = {
-      device = "datapool/isos";
-      fsType = "zfs";
-    };
-    "/mnt/audiobooks" = {
-      device = "datapool/audiobooks";
-      fsType = "zfs";
-    };
-    "/mnt/VMs" = {
-      device = "datapool/vms";
-      fsType = "zfs";
-    };
-    "/mnt/old_backups" = {
-      device = "datapool/old_backups";
-      fsType = "zfs";
-    };
-    "/etc/docker" = {
-      device = "datapool/services";
-      fsType = "zfs";
-    };
-  };
+  # fileSystems = {
+  #   "/mnt/ai" = {
+  #     device = "datapool/ai";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/movies" = {
+  #     device = "datapool/movies";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/tvshows" = {
+  #     device = "datapool/tv_shows";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/music" = {
+  #     device = "datapool/music";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/astrophotography" = {
+  #     device = "datapool/astro";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/downloads" = {
+  #     device = "datapool/downloads";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/photos" = {
+  #     device = "datapool/photos";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/stash" = {
+  #     device = "datapool/stash";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/ISOs" = {
+  #     device = "datapool/isos";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/audiobooks" = {
+  #     device = "datapool/audiobooks";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/VMs" = {
+  #     device = "datapool/vms";
+  #     fsType = "zfs";
+  #   };
+  #   "/mnt/old_backups" = {
+  #     device = "datapool/old_backups";
+  #     fsType = "zfs";
+  #   };
+  #   "/etc/docker" = {
+  #     device = "datapool/services";
+  #     fsType = "zfs";
+  #   };
+  # };
 
   # Install ZFS utilities
   environment.systemPackages = with pkgs; [
